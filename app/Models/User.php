2abb94357078d422ca8be95ef6025aa2ce538f66
zibraque <549713984@qq.com>
2017-09-05 00:46:23 +0800

    master changes

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,5 +5,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
-     protected $fillable = ['name', 'email', 'password'];
+    protected $fillable = ['name', 'email', 'password'];
+	public function gravatar( $size = '100' )
+    {
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return  "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
 }
