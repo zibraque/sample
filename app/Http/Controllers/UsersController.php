04ecea6f6ef9d0d49cf7c11c0c7481bddf7df05f
zibraque <549713984@qq.com>
2017-09-05 23:56:25 +0800

    Finish user signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Models\User;
+
+
 class UsersController extends Controller
 {
     public function create()
@@ -15,5 +17,24 @@ class UsersController extends Controller
     public function show(User $user)
     {
     	return view('users.show',compact('user'));
+    }
+
+    public function store( Request $request )
+    {
+    	// $data = $request->all();
+    	// var_dump($data);die;
+    	$this->validate($request, [
+    		'name' 	   => 'required|max:50',
+    		'email'    => 'required|email|unique:users|max:255',
+    		'password' => 'required'
+    	]);
+
+    	$user = User::create([
+    		'name' => $request->name,
+    		'email' => $request->email,
+    		'password' => bcrypt($request->password),
+    	]);
+    	session()->flash('success','welcome,be fun with a good travel');
+    	return redirect()->route('users.show',[$user]);
     }
 }
