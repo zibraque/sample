3dde861995843d4fdca3501331257cf1572e2154
zibraque <549713984@qq.com>
2017-09-07 00:11:17 +0800

    change port

--- config/database.php
+++ config/database.php
@@ -32,7 +32,7 @@ return [
         'pgsql' => [
             'driver'   => 'pgsql',
             'host'     => $db_config['host'],
-            'port'     => env('DB_PORT', '5432'),
+            'port'     => env('DB_PORT', '3306'),
             'database' => $db_config['database'],
             'username' => $db_config['username'],
             'password' => $db_config['password'],
