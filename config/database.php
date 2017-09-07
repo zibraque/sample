b80d08e854add683f98d838ef0f5d3d53315844c
zibraque <549713984@qq.com>
2017-09-07 23:49:38 +0800

    regist

--- config/database.php
+++ config/database.php
@@ -32,14 +32,14 @@ return [
         'pgsql' => [
             'driver'   => 'pgsql',
             'host'     => $db_config['host'],
-            'port'     => env('DB_PORT', $db_config['port']),
+            'port'     => env('DB_PORT', '5432'),
             'database' => $db_config['database'],
             'username' => $db_config['username'],
             'password' => $db_config['password'],
             'charset'  => 'utf8',
             'prefix'   => '',
             'schema'   => 'public',
-            'sslmode'  => 'require',
+            'sslmode'  => 'prefer',
         ],
 
         'sqlsrv' => [
