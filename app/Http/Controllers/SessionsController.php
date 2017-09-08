b5d6557f998b96044e11f549512588017e09b439
zibraque <549713984@qq.com>
2017-09-08 23:53:56 +0800

    add some changes

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
-use App\Http\Controllers\Auth;
+use Auth;
 class SessionsController extends Controller
 {
     //
